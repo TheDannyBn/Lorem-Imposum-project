626acb7bbe123282cde982f01d2f30a601b6760c
TheDannyBn <dani.baran3@gmail.com>
2024-04-29 17:37:10 +0300

    fix icon favion

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,37 @@
-import { useState } from 'react';
-import data from './data';
-import { nanoid } from 'nanoid';
+import { useState } from "react";
+import data from "./data";
+import { nanoid } from "nanoid";
 
 const App = () => {
   const [count, setCount] = useState(1);
   const [text, setText] = useState([]);
 
-  const handleSubmit = (e) => {
+  const handleSubmit = e => {
     e.preventDefault();
     let amount = parseInt(count);
     setText(data.slice(0, amount));
   };
   return (
-    <section className='section-center'>
+    <section className="section-center">
       <h4>tired of boring lorem ipsum?</h4>
-      <form className='lorem-form' onSubmit={handleSubmit}>
-        <label htmlFor='amount'>paragraphs:</label>
+      <form className="lorem-form" onSubmit={handleSubmit}>
+        <label htmlFor="amount">paragraphs:</label>
         <input
-          type='number'
-          name='amount'
-          id='amount'
-          min='1'
-          step='1'
-          max='8'
+          type="number"
+          name="amount"
+          id="amount"
+          min="1"
+          step="1"
+          max="8"
           value={count}
-          onChange={(e) => setCount(e.target.value)}
+          onChange={e => setCount(e.target.value)}
         />
-        <button className='btn' type='submit'>
+        <button className="btn" type="submit">
           generate
         </button>
       </form>
-      <article className='lorem-text'>
-        {text.map((item) => {
+      <article className="lorem-text">
+        {text.map(item => {
           return <p key={nanoid()}>{item}</p>;
         })}
       </article>
